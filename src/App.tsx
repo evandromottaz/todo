23e6253902246a744001ff61272856de914c63d0
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2022-01-29 01:55:05 -0300

    todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Area, Container, Header} from './App.styles'
+import {useState} from 'react';
+import {Item} from './types/Item'
+import ListItem from './components/ListItem'
+import AddArea from './components/AddArea'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Comprar ovo', done: false},
+    {id: 2, name: 'Comprar sorvete', done: false},
+  ])
+
+  const handleAddTask = (taskName:string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList)
+  }
+
+  const handleTaskChange = (id:number, done:boolean) => {
+    let newList = [...list];
+    console.log(done)
+    for(let i in newList) {
+      if(newList[i].id === id) newList[i].done = done 
+    }
+    setList(newList)
+  }
+
+return (
+<Container>
+  <Area>
+    <Header>Lista de Tarefas</Header>
+
+    {/* Área de adicionar novas tarefas */}
+    <AddArea onEnter={handleAddTask}/>
+
+    {/* Lista de tarefas */}
+    {list.map((item,index) => (
+      <ListItem 
+      key={index} 
+      item={item} 
+      onChange={handleTaskChange}
+      />
+    ))}
+  </Area>
+</Container>
+);
 }
 
 export default App;
